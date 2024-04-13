ce45b4fba186986d90334601bf21544de92204d2
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2024-04-13 20:08:44 +0300

    modal.js

---
+++ js/modal.js
@@ -0,0 +1,40 @@
+// -- скрол до секцій -- //
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// -- таймер у формі -- //
+function startTimer(duration, display) {
+  var timer = duration,
+    hours,
+    minutes,
+    seconds;
+  setInterval(function () {
+    hours = parseInt(timer / 3600, 10);
+    minutes = parseInt((timer % 3600) / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = hours + ":" + minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var twoHours = 2 * 60 * 60;
+  var display = document.getElementById("time");
+  startTimer(twoHours, display);
+};
